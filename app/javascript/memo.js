b92488ba7ba8f306029ae2b5438a4191272f275d
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-07-27 15:41:26 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,15 @@
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+ };
+ 
+ // 第一引数 loadイベント, 第二引数 実行したい処理
+ window.addEventListener('load', post);
