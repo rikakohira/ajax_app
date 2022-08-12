9a8671c8d63af18f1a1251ba088fe714e50886eb
HIRATA RIKAKO <hiratarikako@HIRATAnoMacBook-Air.local>
2022-08-12 14:32:53 +0900

    Rubocop 導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,13 @@
 class PostsController < ApplicationController
-
   def index
-    @posts = Post.order(id: "DESC")
+    @posts = Post.order(id: 'DESC')
   end
 
-  #def new
-  #end
+  # def new
+  # end
 
   def create
     post = Post.create(content: params[:content])
-    render json:{ post: post }
+    render json: { post: post }
   end
 end
